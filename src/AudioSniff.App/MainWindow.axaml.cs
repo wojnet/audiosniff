7f8f5795410ac5034aabf2cdfcdde098d83e7c7a
wojnet <wojnetto@gmail.com>
2025-11-28 22:16:53 +0100

    feat: created AudioRecorder class that records Wasapi Loopback

--- src/AudioSniff.App/MainWindow.axaml.cs
+++ src/AudioSniff.App/MainWindow.axaml.cs
@@ -1,11 +1,20 @@
+using AudioSniff.Core;
+using AudioSniff.Infrastructure;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace AudioSniff.App;
 
 public partial class MainWindow : Window
 {
+    private IAudioRecorder recorder = new AudioRecorder();
+
+    private static string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+    private string filePath = Path.Combine(desktopPath, $"recording-{DateTime.Now.ToString("yyyyMMddHHmm")}.wav");
+
     public MainWindow()
     {
         InitializeComponent();
@@ -16,10 +25,19 @@ public partial class MainWindow : Window
         this.BeginMoveDrag(e);
     }
 
-    private void StartRecording_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    private void Recording_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
-        Debug.WriteLine("Start Recording pressed");
-        // WASAPI
+        if (!recorder.IsRecording)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(filePath)!);
+            recorder.Start(filePath);
+            StartStopButton.Content = "Stop";
+        }
+        else
+        {
+            recorder.Stop();
+            StartStopButton.Content = "Record";
+        }
     }
 
     private void Close_Click(object? sender, RoutedEventArgs e)
---
+++ src/AudioSniff.Infrastructure/AudioRecorder.cs
@@ -0,0 +1,40 @@
+﻿using AudioSniff.Core;
+using NAudio.Wave;
+
+namespace AudioSniff.Infrastructure;
+
+public class AudioRecorder : IAudioRecorder
+{
+    private WasapiLoopbackCapture? capture;
+    private WaveFileWriter? wavWriter;
+
+    private bool isRecording;
+    public bool IsRecording => isRecording;
+
+    public void Start(string filePath)
+    {
+        if (isRecording) return;
+
+        capture = new WasapiLoopbackCapture();
+        wavWriter = new WaveFileWriter(filePath, capture.WaveFormat);
+
+        capture.DataAvailable += (s, e) => wavWriter.Write(e.Buffer, 0, e.BytesRecorded);
+        capture.RecordingStopped += (s, e) =>
+        {
+            wavWriter?.Dispose();
+            capture?.Dispose();
+            wavWriter = null;
+            capture = null;
+            isRecording = false;
+        };
+
+        capture.StartRecording();
+        isRecording = true;
+    }
+
+    public void Stop()
+    {
+        if (!isRecording) return;
+        capture!.StopRecording();
+    }
+}
